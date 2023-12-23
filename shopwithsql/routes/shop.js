e14f8bcd4200c0256b0fee8396a9904699e92017
abooh <abdelrahmankhattab9999@gmail.com>
2023-12-23 08:14:19 +0200

    e-shop where you can add delete surf update a product

--- shopwithsql/routes/shop.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const shopRoutes = express.Router();
-const adminRoutes = require("./admin");
-const productController = require("../controllers/products");
-
-shopRoutes
-  .get("/", productController.shopPageMethod);
-
-module.exports = shopRoutes;
